0168067fbd1715ae8e66b5f08d565c9e10eb77f7
Jesus <suso24jd@gmail.com>
2024-02-15 08:59:28 +0100

    (feat): clase sala: atributos, constructor y get/set hechos

--- src/Sala.java
+++ src/Sala.java
@@ -1,2 +1,36 @@
 public class Sala {
+    private int numero;
+    private Pelicula pelicula;
+    private boolean[][] butacas;
+
+    public Sala(int numero, Pelicula pelicula, int filas, int columnas) {
+        this.numero = numero;
+        this.pelicula = pelicula;
+        this.butacas = new boolean[filas][columnas];
+
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int id) {
+        this.numero = id;
+    }
+
+    public Pelicula getPelicula() {
+        return pelicula;
+    }
+
+    public void setPelicula(Pelicula pelicula) {
+        this.pelicula = pelicula;
+    }
+
+    public boolean[][] getButacas() {
+        return butacas;
+    }
+
+    public void setButacas(boolean[][] butacas) {
+        this.butacas = butacas;
+    }
 }
