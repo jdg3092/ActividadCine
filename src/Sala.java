66854bcae7b7af71eef74177511a3027c6106b88
Jesus <suso24jd@gmail.com>
2024-02-15 10:33:04 +0100

    (doc): documentacion de la clase sala

--- src/Sala.java
+++ src/Sala.java
@@ -1,8 +1,18 @@
 public class Sala {
+    /**
+     * Los atributos de la clase sala, teniendo butacas como una matriz e boolean
+     */
     private int numero;
     private Pelicula pelicula;
     private boolean[][] butacas;
 
+    /**
+     * Contructor para crear en el main la sala
+     * @param numero el numero de la sala
+     * @param pelicula llamamos a la clase Pelicula que se proyectara en la Sala
+     * @param filas numero de filas que tendra la sala en la matriz
+     * @param columnas numero de columas que tendra la sala en la matriz
+     */
     public Sala(int numero, Pelicula pelicula, int filas, int columnas) {
         this.numero = numero;
         this.pelicula = pelicula;
@@ -34,6 +44,13 @@ public class Sala {
         this.butacas = butacas;
     }
 
+    /**
+     * Metodo para reservar la butaca
+     * @param fila el numero de fila de la matriz
+     * @param columna el numero de la columna de la matriz
+     * @return retornara false si fila/columna no existe en la matriz o ya esta ocupada y true si esta vacia.
+     *
+     */
     public boolean reservarButaca(int fila, int columna) {
         if (fila < 0 || fila >= butacas.length || columna < 0 || columna >= butacas[0].length) {
             System.out.println("Error! No existe Butaca ");
@@ -48,6 +65,9 @@ public class Sala {
         return true;
     }
 
+    /**
+     * Metodo que mostrara la matriz con los sitios o libres u ocupados
+     */
     public void mostrarEstadoButacas() {
         System.out.println("Estado de las butacas en la sala " + numero + ":");
         System.out.println("-----------------------------------------");
