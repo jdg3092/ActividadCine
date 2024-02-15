533d3f34ead1427dbf223bc6c2975692d83fa85d
Jesus <suso24jd@gmail.com>
2024-02-15 09:11:13 +0100

    (feat): clase cine: atributos, constructor y get/set hechos

--- src/Cine.java
+++ src/Cine.java
@@ -1,2 +1,45 @@
 public class Cine {
+    private String nombre;
+    private int aforo;
+    private Sala[] salas;
+    private int cantidadSalas;
+
+    public Cine(String nombre, int aforo, int cantidadSalas) {
+        this.nombre = nombre;
+        this.aforo = aforo;
+        this.cantidadSalas = cantidadSalas;
+        this.salas = new Sala[cantidadSalas];
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAforo() {
+        return aforo;
+    }
+
+    public void setAforo(int aforo) {
+        this.aforo = aforo;
+    }
+
+    public Sala[] getSalas() {
+        return salas;
+    }
+
+    public void setSalas(Sala[] salas) {
+        this.salas = salas;
+    }
+
+    public int getCantidadSalas() {
+        return cantidadSalas;
+    }
+
+    public void setCantidadSalas(int cantidadSalas) {
+        this.cantidadSalas = cantidadSalas;
+    }
 }
